55cad337e2c8d946b96a85585ddcddd349516ba9
sm8sk4l <sebastianmoskal@protonmail.com>
2022-10-13 13:43:17 +0200

    lab-2 init

---
+++ lab-2/main.js
@@ -0,0 +1,17 @@
+// notatnik z zajęć
+const main = document.querySelector("main");
+const timeoutRef = setTimeout(() => {
+  main.innerHTML = "From setTimeout";
+}, 20000);
+let licznik = 0;
+const intervalRef = setInterval(() => {
+  main.innerHTML = "From interval" + licznik++;
+}, 4000);
+
+// kasujemy setInterval
+clearInterval(intervalRef);
+
+// kasujemy setTimeout
+clearTimeout(intervalRef);
+
+// window.requestAnimationFrame
