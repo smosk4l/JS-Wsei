1248596578d06c441f5991fd0e9c2eb083ae782a
sm8sk4l <sebastianmoskal@protonmail.com>
2022-11-09 23:07:47 +0100

    added addnote func

--- lab-4/app.js
+++ lab-4/app.js
@@ -2,14 +2,15 @@
 const showFieldBtn = document.querySelector(".show-field-btn");
 const addNoteBtn = document.querySelector(".add-note-btn");
 const addNoteField = document.querySelector(".add-note-field");
+const notesEl = document.querySelector(".notes");
 const noteTitle = document.querySelector("#title");
 const noteContent = document.querySelector("#content");
 const notes = [];
 
-showFieldBtn.addEventListener("click", updateFieldVisibilty);
+showFieldBtn.addEventListener("click", updateFieldVisibility);
 addNoteBtn.addEventListener("click", createNote);
 
-function updateFieldVisibilty() {
+function updateFieldVisibility() {
   addNoteField.classList.toggle("hidden");
   noteTitle.value = "";
   noteContent.value = "";
@@ -18,16 +19,30 @@ function updateFieldVisibilty() {
 function createNote() {
   const title = noteTitle.value;
   const content = noteContent.value;
-  notes.push({
-    title,
-    content,
-  });
-  updateFieldVisibilty();
-  showNoteOnPage();
+  if (!title || !content) alert("Please enter text into field");
+  else {
+    notes.push({
+      title,
+      content,
+    });
+
+    const note = document.createElement("div");
+    const noteTitle = document.createElement("div");
+    const noteContent = document.createElement("div");
+
+    note.classList = "note";
+    noteTitle.className = "note-title";
+    noteContent.className = "note-content";
+
+    noteTitle.textContent = title;
+    noteContent.textContent = content;
+
+    note.appendChild(noteTitle);
+    note.appendChild(noteContent);
+    notesEl.appendChild(note);
+
+    updateFieldVisibility();
+  }
 }
 
-function showNoteOnPage() {
-  console.log(notes.map((x) => x.title));
-  console.log(notes.map((x) => x.content));
-}
-console.log(localStorage);
+function create() {}
