1890aafcc34eadaa2dd74698c5457c55e61dc9b8
sm8sk4l <sebastianmoskal@protonmail.com>
2022-11-21 23:31:42 +0100

    lab-6 init

---
+++ lab-6/app.js
@@ -0,0 +1,12 @@
+window.addEventListener("deviceorientation", onDeviceMove);
+
+function onDeviceMove(event) {
+  console.log(event);
+}
+
+function animate() {
+  //    console.log(Date.now())
+  // requestAnimationFrame(animate)
+}
+
+requestAnimationFrame(animate);
