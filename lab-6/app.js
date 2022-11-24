1161e2e824ec0162afc40dbebc093a0e157f6d75
sm8sk4l <sebastianmoskal@protonmail.com>
2022-11-24 03:31:21 +0100

    draw circles with overlap done

--- lab-6/app.js
+++ lab-6/app.js
@@ -1,12 +1,43 @@
-window.addEventListener("deviceorientation", onDeviceMove);
+"use strict";
 
-function onDeviceMove(event) {
-  console.log(event);
+const canvas = document.querySelector("canvas");
+const ctx = canvas.getContext("2d");
+const circles = [];
+
+drawCircles();
+function drawCircles() {
+  for (let i = 0; i < 100; i++) {
+    const circle = {
+      width: Math.random() * canvas.width,
+      height: Math.random() * canvas.height,
+      radious: 12,
+    };
+
+    circles.push(circle);
+  }
+
+  for (let i = 0; i < circles.length; i++) {
+    ctx.beginPath();
+    ctx.arc(
+      circles[i].width,
+      circles[i].height,
+      circles[i].radious,
+      0,
+      Math.PI * 2
+    );
+    ctx.fill();
+  }
 }
 
-function animate() {
-  //    console.log(Date.now())
-  // requestAnimationFrame(animate)
-}
+// window.addEventListener("deviceorientation", onDeviceMove);
 
-requestAnimationFrame(animate);
+// function onDeviceMove(event) {
+//   console.log(event);
+// }
+
+// function animate() {
+//   //    console.log(Date.now())
+//   // requestAnimationFrame(animate)
+// }
+
+// requestAnimationFrame(animate);
