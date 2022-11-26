982817212fbd88f197996236d5f932928f66a1d3
sm8sk4l <sebastianmoskal@protonmail.com>
2022-11-26 23:51:26 +0100

    cleaned code

--- lab-6/app.js
+++ lab-6/app.js
@@ -6,11 +6,11 @@ const circles = [];
 
 drawCircles();
 function drawCircles() {
-  while (circles.length < 250) {
+  while (circles.length < 100) {
     const circle = {
       width: Math.floor(Math.random() * canvas.width),
       height: Math.floor(Math.random() * canvas.height),
-      radious: 12,
+      radious: 6,
     };
 
     let isOverlapping = false;
@@ -43,40 +43,12 @@ function drawCircles() {
     );
     ctx.fill();
   }
-  // for (let i = 0; i < circles.length; i++) {
-  //   if (i > 0) {
-  //     const d = getDistance(
-  //       circles[i - 1].width,
-  //       circles[i - 1].height,
-  //       circles[i].width,
-  //       circles[i].height
-  //     );
-  //     if (d > circles[0].radious / 2) {
-  // ctx.beginPath();
-  // ctx.arc(
-  //   circles[i].width,
-  //   circles[i].height,
-  //   circles[i].radious,
-  //   0,
-  //   Math.PI * 2
-  // );
-  // ctx.fill();
-  //     }
-  //   }
-  // }
 }
-function getDistance(x1, y1, x2, y2) {
-  let y = x2 - x1;
-  let x = y2 - y1;
+window.addEventListener("deviceorientation", onDeviceMove);
 
-  return Math.sqrt(x * x + y * y);
+function onDeviceMove(event) {
+  console.log(event);
 }
-
-// window.addEventListener("deviceorientation", onDeviceMove);
-
-// function onDeviceMove(event) {
-//   console.log(event);
-// }
 
 // function animate() {
 //   //    console.log(Date.now())
