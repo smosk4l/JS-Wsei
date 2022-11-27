3d1d84e357eb05b06aaa620eb474687bd1ceb84d
sm8sk4l <sebastianmoskal@protonmail.com>
2022-11-27 23:47:18 +0100

    moving ball by mouse

--- lab-6/app.js
+++ lab-6/app.js
@@ -4,13 +4,32 @@ const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
 const circles = [];
 
-drawCircles();
+let mouse = {
+  x: innerWidth / 2,
+  y: innerHeight / 2,
+};
+
+canvas.addEventListener("mousemove", function (event) {
+  mouse.x = event.clientX;
+  mouse.y = event.clientY;
+  draw(mouse.x, mouse.y, 12);
+});
+
+function draw(x, y, radius) {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.beginPath();
+  ctx.arc(x, y, radius, 0, Math.PI * 2);
+  ctx.fill();
+  ctx.closePath();
+}
+
+// drawCircles();
 function drawCircles() {
   while (circles.length < 100) {
     const circle = {
       width: Math.floor(Math.random() * canvas.width),
       height: Math.floor(Math.random() * canvas.height),
-      radious: 6,
+      radius: 6,
     };
 
     let isOverlapping = false;
@@ -21,7 +40,7 @@ function drawCircles() {
         circle.height - previousCircle.height
       );
 
-      if (distance < circle.radious + previousCircle.radious) {
+      if (distance < circle.radius + previousCircle.radius) {
         isOverlapping = true;
         break;
       }
@@ -37,7 +56,7 @@ function drawCircles() {
     ctx.arc(
       circles[i].width,
       circles[i].height,
-      circles[i].radious,
+      circles[i].radius,
       0,
       Math.PI * 2
     );
@@ -49,10 +68,5 @@ window.addEventListener("deviceorientation", onDeviceMove);
 function onDeviceMove(event) {
   console.log(event);
 }
-
-// function animate() {
-//   //    console.log(Date.now())
-//   // requestAnimationFrame(animate)
-// }
 
 // requestAnimationFrame(animate);
