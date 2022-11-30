ce088b3dda783bef90ea9a5e5fc65affd1c49c58
sm8sk4l <sebastianmoskal@protonmail.com>
2022-11-30 18:50:12 +0100

    moving balls done

--- lab-6/app.js
+++ lab-6/app.js
@@ -2,71 +2,70 @@
 
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
-const circles = [];
+const balls = [];
 
-let mouse = {
-  x: innerWidth / 2,
-  y: innerHeight / 2,
-};
+const width = window.innerWidth;
+const height = window.innerHeight;
 
-canvas.addEventListener("mousemove", function (event) {
-  mouse.x = event.clientX;
-  mouse.y = event.clientY;
-  draw(mouse.x, mouse.y, 12);
-});
+canvas.width = width;
+canvas.height = height;
 
-function draw(x, y, radius) {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-  ctx.beginPath();
-  ctx.arc(x, y, radius, 0, Math.PI * 2);
-  ctx.fill();
-  ctx.closePath();
-}
-
-// drawCircles();
-function drawCircles() {
-  while (circles.length < 100) {
-    const circle = {
-      width: Math.floor(Math.random() * canvas.width),
-      height: Math.floor(Math.random() * canvas.height),
-      radius: 6,
-    };
-
-    let isOverlapping = false;
-    for (let i = 0; i < circles.length; i++) {
-      const previousCircle = circles[i];
-      const distance = Math.hypot(
-        circle.width - previousCircle.width,
-        circle.height - previousCircle.height
-      );
-
-      if (distance < circle.radius + previousCircle.radius) {
-        isOverlapping = true;
-        break;
-      }
-    }
-
-    if (!isOverlapping) {
-      circles.push(circle);
-    }
+class Ball {
+  constructor(x, y, speedX, speedY, radius, color) {
+    this.x = x;
+    this.y = y;
+    this.speedX = speedX;
+    this.speedY = speedY;
+    this.radius = radius;
+    this.color = color;
   }
 
-  for (let i = 0; i < circles.length; i++) {
+  drawBall() {
     ctx.beginPath();
-    ctx.arc(
-      circles[i].width,
-      circles[i].height,
-      circles[i].radius,
-      0,
-      Math.PI * 2
-    );
+    ctx.fillStyle = this.color;
+    ctx.arc(this.x, this.y, this.radius, 0, 2 * Math.PI);
     ctx.fill();
   }
-}
-window.addEventListener("deviceorientation", onDeviceMove);
 
-function onDeviceMove(event) {
-  console.log(event);
+  updateBall() {
+    if (this.x + this.radius >= width || this.x - this.radius <= 0) {
+      this.speedX = -this.speedX;
+    }
+
+    if (this.y + this.radius >= height || this.y - this.radius <= 0) {
+      this.speedY = -this.speedY;
+    }
+
+    this.x += this.speedX;
+    this.y += this.speedY;
+  }
 }
 
-// requestAnimationFrame(animate);
+function random(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+while (balls.length < 10) {
+  const radius = random(10, 20);
+  const ball = new Ball(
+    random(radius, width - radius),
+    random(radius, height - radius),
+    random(-10, 10),
+    random(-10, 10),
+    radius,
+    `rgb(${random(0, 255)}, ${random(0, 255)}, ${random(0, 255)})`
+  );
+  balls.push(ball);
+}
+
+function loop() {
+  ctx.fillStyle = "rgba(0,0,0)";
+  ctx.fillRect(0, 0, width, height);
+  for (let i = 0; i < balls.length; i++) {
+    balls[i].drawBall();
+    balls[i].updateBall();
+  }
+  requestAnimationFrame(loop);
+}
+
+loop();
