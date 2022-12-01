a717a455ac10ab9ddf19c387d6863ee4da48d9de
sm8sk4l <sebastianmoskal@protonmail.com>
2022-12-01 23:56:21 +0100

    ball stopping on the edge done

--- lab-6/app.js
+++ lab-6/app.js
@@ -3,12 +3,14 @@
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
 const balls = [];
-
 const width = window.innerWidth;
 const height = window.innerHeight;
 
 canvas.width = width;
 canvas.height = height;
+
+let beta = 0;
+let gamma = 0;
 
 class Ball {
   constructor(x, y, speedX, speedY, radius, color) {
@@ -39,13 +41,26 @@ class Ball {
     this.x += this.speedX;
     this.y += this.speedY;
   }
+
+  updatePlayerBall() {
+    {
+      if (this.x + this.speedX >= width || this.x + this.speedX <= 0) {
+        this.speedX = 0;
+      }
+      if (this.y + this.speedY >= height || this.y + this.speedY <= 0) {
+        this.speedY = 0;
+      }
+      this.x += this.speedX;
+      this.y += this.speedY;
+    }
+  }
 }
 
 function random(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-while (balls.length < 10) {
+while (balls.length < 1) {
   const radius = random(10, 20);
   const ball = new Ball(
     random(radius, width - radius),
@@ -58,6 +73,24 @@ while (balls.length < 10) {
   balls.push(ball);
 }
 
+let playerBall = new Ball(
+  random(20, width - 20),
+  random(20, height - 20),
+  gamma,
+  beta,
+  20,
+  "#fff"
+);
+
+playerBall.drawBall();
+
+window.addEventListener("deviceorientation", function (event) {
+  beta = event.beta / 10;
+  gamma = event.gamma / 5;
+  playerBall.speedX = gamma;
+  playerBall.speedY = beta;
+});
+
 function loop() {
   ctx.fillStyle = "rgba(0,0,0)";
   ctx.fillRect(0, 0, width, height);
@@ -65,6 +98,8 @@ function loop() {
     balls[i].drawBall();
     balls[i].updateBall();
   }
+  playerBall.drawBall();
+  playerBall.updatePlayerBall();
   requestAnimationFrame(loop);
 }
 
