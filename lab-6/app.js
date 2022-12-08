ac2a3aa314c4b7fb1c6b48ce2a95638fc7b3bd51
sm8sk4l <sebastianmoskal@protonmail.com>
2022-12-08 23:49:32 +0100

    refactored code

--- lab-6/app.js
+++ lab-6/app.js
@@ -1,11 +1,10 @@
 "use strict";
 
+import { Ball, ctx, width, height } from "./ball.js";
+
 const playBtn = document.querySelector(".btn-play");
 const highscore = document.querySelector(".highscore");
 const menu = document.querySelector(".menu");
-
-const canvas = document.querySelector("canvas");
-const ctx = canvas.getContext("2d");
 
 const ballsAmount = 1;
 
@@ -20,61 +19,13 @@ let gamma = 0;
 
 let isPlaying = true;
 
-const width = window.innerWidth;
-const height = window.innerHeight;
-
-canvas.width = width;
-canvas.height = height;
-
-class Ball {
-  constructor(x, y, speedX, speedY, radius, color) {
-    this.x = x;
-    this.y = y;
-    this.speedX = speedX;
-    this.speedY = speedY;
-    this.radius = radius;
-    this.color = color;
-  }
-
-  drawBall() {
-    ctx.beginPath();
-    ctx.fillStyle = this.color;
-    ctx.arc(this.x, this.y, this.radius, 0, 2 * Math.PI);
-    ctx.fill();
-  }
-
-  updateBall(type = "normal") {
-    if (type === "normal") {
-      if (this.x + this.radius >= width || this.x - this.radius <= 0) {
-        this.speedX = -this.speedX;
-      }
-
-      if (this.y + this.radius >= height || this.y - this.radius <= 0) {
-        this.speedY = -this.speedY;
-      }
-
-      this.x += this.speedX;
-      this.y += this.speedY;
-    } else {
-      if (this.x + this.speedX >= width || this.x + this.speedX <= 0) {
-        this.speedX = 0;
-      }
-      if (this.y + this.speedY >= height || this.y + this.speedY <= 0) {
-        this.speedY = 0;
-      }
-      this.x += this.speedX;
-      this.y += this.speedY;
-    }
-  }
-
-  removeBallFromArr() {
-    balls = balls.filter((ball) => {
-      return (
-        Math.hypot(playerBall.x - ball.x, playerBall.y - ball.y) >=
-        playerBall.radius + ball.radius
-      );
-    });
-  }
+function removeBallFromArr() {
+  balls = balls.filter((ball) => {
+    return (
+      Math.hypot(playerBall.x - ball.x, playerBall.y - ball.y) >=
+      playerBall.radius + ball.radius
+    );
+  });
 }
 
 function random(min, max) {
@@ -84,6 +35,14 @@ function random(min, max) {
 function startGame() {
   isPlaying = true;
   times[0] = Date.now();
+
+  createHoles();
+  createPlayerBall();
+
+  loop();
+}
+
+function createHoles() {
   while (balls.length < ballsAmount) {
     const radius = random(10, 20);
     const ball = new Ball(
@@ -96,8 +55,11 @@ function startGame() {
     );
     balls.push(ball);
   }
+}
 
+function createPlayerBall() {
   const radius = 50;
+
   playerBall = new Ball(
     random(radius, width - radius),
     random(radius, height - radius),
@@ -106,10 +68,7 @@ function startGame() {
     radius,
     "#fff"
   );
-
-  loop();
 }
-
 window.addEventListener("deviceorientation", function (event) {
   beta = event.beta / 5;
   gamma = event.gamma / 5;
@@ -118,29 +77,24 @@ window.addEventListener("deviceorientation", function (event) {
 });
 
 function loop() {
+  clearBackground();
+  checkIfGameOver();
+
+  if (isPlaying) {
+    for (let i = 0; i < balls.length; i++) {
+      balls[i].drawBall();
+      balls[i].updateBall();
+      removeBallFromArr();
+    }
+    playerBall.drawBall();
+    playerBall.updateBall("player");
+  }
+  requestAnimationFrame(loop);
+}
+
+function clearBackground() {
   ctx.fillStyle = "bisque";
   ctx.fillRect(0, 0, width, height);
-
-  if (balls.length === 0) {
-    isPlaying = false;
-    times[1] = Date.now();
-    timeDiff.push((times[1] - times[0]) / 1000);
-    setHighscore(Math.min(...timeDiff));
-  }
-
-  isPlaying ? (menu.style.display = "none") : (menu.style.display = "flex");
-
-  if (!isPlaying) return;
-
-  for (let i = 0; i < balls.length; i++) {
-    balls[i].drawBall();
-    balls[i].updateBall();
-    balls[i].removeBallFromArr();
-  }
-  playerBall.drawBall();
-  playerBall.updateBall("player");
-
-  requestAnimationFrame(loop);
 }
 
 function setHighscore(time) {
@@ -149,4 +103,17 @@ function setHighscore(time) {
   highscore.textContent = `${minutes}:${seconds}`;
 }
 
+function checkIfGameOver() {
+  if (balls.length === 0) {
+    times[1] = Date.now();
+    timeDiff.push((times[1] - times[0]) / 1000);
+    setHighscore(Math.min(...timeDiff));
+    menu.style.display = "flex";
+    isPlaying = false;
+    return;
+  } else {
+    menu.style.display = "none";
+    return;
+  }
+}
 playBtn.addEventListener("click", startGame);
---
+++ lab-6/ball.js
@@ -0,0 +1,52 @@
+"use strict";
+const canvas = document.querySelector("canvas");
+const ctx = canvas.getContext("2d");
+
+const width = window.innerWidth;
+const height = window.innerHeight;
+
+canvas.width = width;
+canvas.height = height;
+
+class Ball {
+  constructor(x, y, speedX, speedY, radius, color) {
+    this.x = x;
+    this.y = y;
+    this.speedX = speedX;
+    this.speedY = speedY;
+    this.radius = radius;
+    this.color = color;
+  }
+
+  drawBall() {
+    ctx.beginPath();
+    ctx.fillStyle = this.color;
+    ctx.arc(this.x, this.y, this.radius, 0, 2 * Math.PI);
+    ctx.fill();
+  }
+
+  updateBall(type = "normal") {
+    if (type === "normal") {
+      if (this.x + this.radius >= width || this.x - this.radius <= 0) {
+        this.speedX = -this.speedX;
+      }
+
+      if (this.y + this.radius >= height || this.y - this.radius <= 0) {
+        this.speedY = -this.speedY;
+      }
+
+      this.x += this.speedX;
+      this.y += this.speedY;
+    } else {
+      if (this.x + this.speedX >= width || this.x + this.speedX <= 0) {
+        this.speedX = 0;
+      }
+      if (this.y + this.speedY >= height || this.y + this.speedY <= 0) {
+        this.speedY = 0;
+      }
+      this.x += this.speedX;
+      this.y += this.speedY;
+    }
+  }
+}
+export { Ball, canvas, ctx, width, height };
