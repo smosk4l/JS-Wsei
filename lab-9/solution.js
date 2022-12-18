db87bc93f5cf0d9db0b6d15f6e90aabe4d4843b6
sm8sk4l <sebastianmoskal@protonmail.com>
2022-12-18 23:56:11 +0100

    basic css

--- lab-9/solution.js
+++ lab-9/solution.js
@@ -1,10 +1,18 @@
-const apiKey = "1a42eb189c06352349faade92b23e721";
-const city = "Cracow";
+const checkBtn = document.querySelector(".weather-btn");
+const cityInput = document.querySelector(".weather-input");
 
-console.log(
+const apiKey = "1a42eb189c06352349faade92b23e721";
+
+checkBtn.addEventListener("click", checkWeather);
+function checkWeather() {
+  const city = cityInput.value;
+  cityInput.value = "";
+
   fetch(
-    `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`
+    `https://api.openweathermap.org/data/2.5/weather?units=metric&q=${city}&appid=${apiKey}`
   )
     .then((response) => response.json())
-    .then((data) => console.log(data))
-);
+    .then((data) => {
+      if (data.cod === 200) console.log(data);
+    });
+}
