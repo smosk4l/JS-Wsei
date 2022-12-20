eeecaa7366f153a72dea220b02333e57d88cf31a
sm8sk4l <sebastianmoskal@protonmail.com>
2022-12-20 18:34:57 +0100

    updating UI almost done

--- lab-9/solution.js
+++ lab-9/solution.js
@@ -1,4 +1,4 @@
-const checkBtn = document.querySelector(".weather-btn");
+const checkBtn = document.querySelector(".search-btn");
 const cityInput = document.querySelector(".weather-input");
 
 const apiKey = "1a42eb189c06352349faade92b23e721";
@@ -7,14 +7,27 @@ checkBtn.addEventListener("click", checkWeather);
 function checkWeather() {
   const city = cityInput.value;
   cityInput.value = "";
-  cityInput.
-
   fetch(
     `https://api.openweathermap.org/data/2.5/weather?units=metric&q=${city}&appid=${apiKey}`
   )
     .then((response) => response.json())
-    .then((data) => {
-      if (data.cod === 200) console.log(data);
-    });
+    .then((data) => updateWeather(data));
 }
 
+function updateWeather(data) {
+  const cityName = data.name;
+  const { temp } = data.main;
+  // const icon =
+
+  const weatherHtml = `
+  <div class="card">
+    <h2 class="city">Weather in ${data.name}</h2>
+    <div class="temp">${data}°C</div>
+    <img src="" alt="" class="weather-icon" />
+    <div class="description">Snow</div>
+    <div class="humidity">Humidity: 60%</div>
+    <div class="wind">Wind speed: 6.2 km/h</div>
+</div>`;
+
+  console.log(temp);
+}
