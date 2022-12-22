64676a59e1a274ad45257f5d9c4d54522d2db38e
sm8sk4l <sebastianmoskal@protonmail.com>
2022-12-22 02:18:45 +0100

    more css

--- lab-9/solution.js
+++ lab-9/solution.js
@@ -1,9 +1,11 @@
 const checkBtn = document.querySelector(".search-btn");
 const cityInput = document.querySelector(".weather-input");
+const weatherEl = document.querySelector(".weather");
 
 const apiKey = "1a42eb189c06352349faade92b23e721";
 
 checkBtn.addEventListener("click", checkWeather);
+
 function checkWeather() {
   const city = cityInput.value;
   cityInput.value = "";
@@ -11,23 +13,33 @@ function checkWeather() {
     `https://api.openweathermap.org/data/2.5/weather?units=metric&q=${city}&appid=${apiKey}`
   )
     .then((response) => response.json())
-    .then((data) => updateWeather(data));
+    .then((data) => updateWeatherUI(data));
 }
 
-function updateWeather(data) {
+function updateWeatherUI(data) {
   const cityName = data.name;
-  const { temp } = data.main;
-  // const icon =
+  const [{ icon }] = data.weather;
+  const { temp, humidity } = data.main;
+  const { speed } = data.wind;
 
-  const weatherHtml = `
+  let [{ description }] = data.weather;
+  description = description
+    .split(" ")
+    .map((word) => word[0].toUpperCase() + word.substring(1))
+    .join(" ");
+
+  const html = `
   <div class="card">
-    <h2 class="city">Weather in ${data.name}</h2>
-    <div class="temp">${data}°C</div>
-    <img src="" alt="" class="weather-icon" />
-    <div class="description">Snow</div>
-    <div class="humidity">Humidity: 60%</div>
-    <div class="wind">Wind speed: 6.2 km/h</div>
-</div>`;
+    <button class="delete-btn">
+      <ion-icon class="icon close-icon" name="close-outline"></ion-icon>
+    </button>
+    <h2 class="city">Weather in ${cityName}</h2>
+    <div class="temp">${Math.round(temp)}°C</div>
+    <img src="https://openweathermap.org/img/wn/${icon}@2x.png" alt="" class="weather-icon" />
+    <div class="description">Description: ${description}</div>
+    <div class="humidity">Humidity: ${humidity}%</div>
+    <div class="wind">Wind speed: ${speed} km/h</div>
+  </?div>`;
 
-  console.log(temp);
+  weatherEl.insertAdjacentHTML("afterbegin", html);
 }
