e91390fa37f8a20e07c4bdde4ba78541b1c8003d
sm8sk4l <sebastianmoskal@protonmail.com>
2023-01-14 10:24:37 +0100

    html && css rdy

---
+++ lab-4/note.js
@@ -0,0 +1,9 @@
+export default class Note {
+  constructor(name, content, color, pinned, date) {
+    this.name = name;
+    this.content = content;
+    this.color = color;
+    this.pinned = pinned;
+    this.date = date;
+  }
+}
