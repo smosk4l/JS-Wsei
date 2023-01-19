2541a51d18d4f98df6cd92422aa3f33d75714437
sm8sk4l <sebastianmoskal@protonmail.com>
2023-01-19 22:40:48 +0100

    removeNote fixed && deleting from local by ID

--- lab-4/app.js
+++ lab-4/app.js
@@ -3,9 +3,17 @@ const noteTitleField = document.querySelector(".note--title");
 const noteContentField = document.querySelector(".note--content");
 const notesListEl = document.querySelector(".notes__list");
 
-let removeNoteBtn = document.querySelector(".btn-remove-note");
+notesListEl.addEventListener("click", removeNote);
 
-const notes = JSON.parse(localStorage.getItem("user-notes") || "[]");
+const notes = getNotes();
+
+init();
+
+function init() {
+  notes.forEach((note) => {
+    updateUI(note);
+  });
+}
 
 function getNotes() {
   return JSON.parse(localStorage.getItem("user-notes") || "[]");
@@ -39,16 +47,9 @@ function saveNote() {
   localStorage.setItem("user-notes", JSON.stringify(notes));
 }
 
-function init() {
-  notes.forEach((note) => {
-    updateUI(note);
-  });
-  getRemoveBtnsAndSetEvent();
-}
-
 function updateUI(note) {
   const html = `
-        <div class="notes__list--item notes__list-item--selected">
+        <div id="${note.id}"class="notes__list--item notes__list-item--selected">
           <div class="note-btns">
             <button class="btn btn-pin-note">📌</button>
             <button class="btn btn-remove-note">❌</button>
@@ -67,28 +68,24 @@ function clearFields() {
   noteContentField.value = "";
 }
 
-function getRemoveBtnsAndSetEvent() {
-  document
-    .querySelectorAll(".btn-remove-note")
-    .forEach((btn) => btn.addEventListener("click", removeNote));
-}
-
 function addNote() {
   createNote();
   saveNote();
-  getRemoveBtnsAndSetEvent();
   clearFields();
 }
 
 function removeNote(e) {
-  let notes = getNotes();
+  if (e.target.className.includes("btn-remove-note")) {
+    const noteEl = e.target.parentNode.parentNode;
+    const noteID = noteEl.id;
 
-  const noteEl = e.currentTarget.parentNode.parentNode;
-  const noteBody = noteEl.querySelector(".note__small--content").textContent;
+    let notes = getNotes();
 
-  notes = notes.filter((note) => note.content !== noteBody);
-  localStorage.setItem("user-notes", JSON.stringify(notes));
-  noteEl.remove();
+    notes = notes.filter((note) => note.id !== noteID);
+    localStorage.setItem("user-notes", JSON.stringify(notes));
+
+    noteEl.remove();
+  }
 }
 
 function removeAllChildNodes(parent) {
@@ -98,10 +95,3 @@ function removeAllChildNodes(parent) {
 }
 
 addNoteBtn.addEventListener("click", addNote);
-// removeNoteBtn.addEventListener("click", removeNote);
-
-// TO DO
-// 1. Zrobic funkcje create note, która tworzy notatke z klas
-// 2. Zmienic funcke save note, na zapisywanie do local stora
-// 3. Zrobic funckcje init, ktora wyswietli wszystkie notsy  z local storage, da im removenote event,
-init();
